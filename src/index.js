7ac6cae7ee5f64bf4da6ba353b11439e69129283
Danivaldo Sousa <danivaldosousa@192.168.1.10>
2021-04-18 00:30:45 -0300

    First commit with Middleware all right

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const { v4: uuidv4, validate } = require('uuid');
 
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -10,19 +11,72 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find( (user) => user.username === username);
+
+  if (!user) {
+
+   return response.status(404).json({ error: "User not found!"});
+    
+  }
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(!user.pro) {
+     if(user.todos.length > 9 ){
+        return response.status(403).json({ error: "free users can create a maximum of 10 todos"});
+     }
+  }
+
+  return next();
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find( (user) => user.username === username );
+
+  if(!user){
+       return response.status(404).json({ error: "User not found!"});
+  }
+
+  if(!validate(id)){
+     return response.status(400).json({ error: "The id must of type uuid"});
+  }
+
+  const todo = user.todos.find( todo => todo.id === id);
+
+  if(!todo){
+        return response.status(404).json({ error: "ID not found!"});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+    const { id } = request.params;
+
+	const user = users.find(user => user.id === id);
+
+	if(!user) {
+		return response.status(404).json({ error: 'User not found' });
+	};
+
+	request.user = user;
+
+	return next();
 }
 
 app.post('/users', (request, response) => {
